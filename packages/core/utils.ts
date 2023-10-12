16b0037b6f722099fa955ca95464108d12cb966a
Moosa Shah <hello.moosashah@gmail.com>
2023-10-12 14:29:19 +0100

    move functions into utils file

---
+++ packages/core/utils.ts
@@ -0,0 +1,170 @@
+import { CreateGame, CreateSelectMenu, Item, TeamKey, teams } from "./types";
+import AWS from "aws-sdk";
+import Game from "../core/database/game";
+import { Queue } from "sst/node/queue";
+import { Config } from "sst/node/config";
+
+const sqs = new AWS.SQS();
+export function createVotingSelectMenu({
+  games,
+  title,
+  customId,
+  placeholder,
+}: CreateSelectMenu) {
+  const components = games.data.map((game) => ({
+    label: `${teams[game.red_side.team_name as TeamKey]} vs ${
+      teams[game.blue_side.team_name as TeamKey]
+    }`,
+    value: game.game_id,
+    description: `${teams[game.red_side.team_name as TeamKey]} vs ${
+      teams[game.blue_side.team_name as TeamKey]
+    }`,
+  }));
+
+  return {
+    content: title,
+    components: [
+      {
+        type: 1,
+        components: [
+          {
+            type: 3,
+            custom_id: customId,
+            options: components,
+            placeholder,
+          },
+        ],
+      },
+    ],
+  };
+}
+
+export function createPointsSelectMenu({
+  games,
+  title,
+  customId,
+  placeholder,
+}: CreateSelectMenu) {
+  const components = games.data.flatMap((game) => {
+    const redSide = {
+      label: `${teams[game.red_side.team_name as TeamKey]} vs ${
+        teams[game.blue_side.team_name as TeamKey]
+      }: ${teams[game.red_side.team_name as TeamKey]}`,
+      value: `${game.game_id}#${game.red_side}`,
+      description: `${teams[game.red_side.team_name as TeamKey]} vs ${
+        teams[game.blue_side.team_name as TeamKey]
+      }: ${teams[game.red_side.team_name as TeamKey]}`,
+    };
+
+    const blueSide = {
+      label: `${teams[game.red_side.team_name as TeamKey]} vs ${
+        teams[game.blue_side.team_name as TeamKey]
+      }: ${teams[game.blue_side.team_name as TeamKey]}`,
+      value: `${game.game_id}#${game.blue_side}`,
+      description: `${teams[game.blue_side.team_name as TeamKey]} vs ${
+        teams[game.blue_side.team_name as TeamKey]
+      }: ${teams[game.blue_side.team_name as TeamKey]}`,
+    };
+    return [redSide, blueSide];
+  });
+
+  return {
+    content: title,
+    components: [
+      {
+        type: 1,
+        components: [
+          {
+            type: 3,
+            custom_id: customId,
+            options: components,
+            placeholder,
+          },
+        ],
+      },
+    ],
+  };
+}
+
+export const extractGameId = (str: string) =>
+  (str.match(/\$vote\/(.*?)#/) || [])[1];
+export const extractPickId = (str: string) => (str.match(/#(.*)$/) || [])[1];
+
+export const createGameInDB = async (rd: string, blu: string) =>
+  (
+    await Game.create({
+      red_side: { team_name: rd },
+      blue_side: { team_name: blu },
+    })
+  ).data.game_id;
+
+export const createGame = async (body: CreateGame) => {
+  const { options } = body.data;
+  const blu = options.find((s) => s.name === "blue_side");
+  const rd = options.find((s) => s.name === "red_side");
+  if (!blu || !rd) {
+    return { content: "Could not create game, missing teams" };
+  }
+  if (blu.value === rd.value) {
+    return { content: "Teams cannot play themselves" };
+  }
+  const game_id = await createGameInDB(rd.value, blu.value);
+
+  const opts = options.map((t) => ({
+    type: 2,
+    style: 2,
+    label: teams[t.value],
+    custom_id: `$vote/${game_id}#${t.name}`,
+  }));
+
+  const components = [
+    {
+      type: 1,
+      components: opts,
+    },
+  ];
+
+  return {
+    content: `Match: ${teams[blu.value]} vs ${teams[rd.value]}`,
+    components,
+  };
+};
+
+export const sendToVotesQueue = async (item: Item) => {
+  const params: AWS.SQS.SendMessageRequest = {
+    QueueUrl: Queue.VotesQueue.queueUrl,
+    MessageBody: JSON.stringify(item),
+  };
+
+  try {
+    await sqs.sendMessage(params).promise();
+  } catch (e) {
+    console.error("error sending batch to votesQueue: ", e);
+    throw e;
+  }
+};
+
+interface Event {
+  headers: {
+    [key: string]: string | undefined;
+  };
+  body: string | null;
+}
+
+export const authenticate = (event: Event, nacl: any): Boolean => {
+  const sig =
+    event.headers["x-signature-ed25519"] ||
+    event.headers["X-Signature-Ed25519"];
+  const ts =
+    event.headers["x-signature-timestamp"] ||
+    event.headers["X-Signature-Timestamp"];
+  if (!sig || !ts || !event.body) {
+    return false;
+  }
+
+  return nacl.sign.detached.verify(
+    Buffer.from(ts + event.body),
+    Buffer.from(sig, "hex"),
+    Buffer.from(Config.PUBLIC_KEY, "hex")
+  );
+};
--- packages/functions/src/interactions/interactions.ts
+++ packages/functions/src/interactions/interactions.ts
@@ -1,94 +1,24 @@
 import { APIGatewayEvent } from "aws-lambda";
 import { InteractionResponseType, InteractionType } from "discord-interactions";
 import AWS from "aws-sdk";
-import { Queue } from "sst/node/queue";
 import { Function } from "sst/node/function";
-import { Config } from "sst/node/config";
-import Game, { type GetActiveGameResponse } from "@pickems/core/database/game";
+import Game from "@pickems/core/database/game";
 import { CreateGame, Item, ParsedBody } from "@pickems/core/types";
+import {
+  authenticate,
+  createGame,
+  createVotingSelectMenu,
+  extractGameId,
+  extractPickId,
+  sendToVotesQueue,
+} from "@pickems/core/utils";
 import nacl from "tweetnacl";
-const sqs = new AWS.SQS();
 const lambda = new AWS.Lambda();
-
-const extractGameId = (str: string) => (str.match(/\$vote\/(.*?)#/) || [])[1];
-const extractPickId = (str: string) => (str.match(/#(.*)$/) || [])[1];
-
-const createGameInDB = async (rd: string, blu: string) =>
-  (
-    await Game.create({
-      red_side: { team_name: rd },
-      blue_side: { team_name: blu },
-    })
-  ).data.game_id;
-
-const createGame = async (body: CreateGame) => {
-  const { options } = body.data;
-  const blu = options.find((s) => s.name === "blue_side");
-  const rd = options.find((s) => s.name === "red_side");
-  if (!blu || !rd) {
-    return { content: "Could not create game, missing teams" };
-  }
-  if (blu.value === rd.value) {
-    return { content: "Teams cannot play themselves" };
-  }
-  const game_id = await createGameInDB(rd.value, blu.value);
-
-  const opts = options.map((t) => ({
-    type: 2,
-    style: 2,
-    label: teams[t.value],
-    custom_id: `$vote/${game_id}#${t.name}`,
-  }));
-
-  const components = [
-    {
-      type: 1,
-      components: opts,
-    },
-  ];
-
-  return {
-    content: `Match: ${teams[blu.value]} vs ${teams[rd.value]}`,
-    components,
-  };
-};
-
-const sendToSQS = async (item: Item) => {
-  const params: AWS.SQS.SendMessageRequest = {
-    QueueUrl: Queue.VotesQueue.queueUrl,
-    MessageBody: JSON.stringify(item),
-  };
-
-  try {
-    await sqs.sendMessage(params).promise();
-  } catch (e) {
-    console.error("error sending batch to votesQueue: ", e);
-    throw e;
-  }
-};
-
-const authenticate = (event: APIGatewayEvent): Boolean => {
-  const sig =
-    event.headers["x-signature-ed25519"] ||
-    event.headers["X-Signature-Ed25519"];
-  const ts =
-    event.headers["x-signature-timestamp"] ||
-    event.headers["X-Signature-Timestamp"];
-  if (!sig || !ts || !event.body) {
-    return false;
-  }
-
-  return nacl.sign.detached.verify(
-    Buffer.from(ts + event.body),
-    Buffer.from(sig, "hex"),
-    Buffer.from(Config.PUBLIC_KEY, "hex")
-  );
-};
 
 export const main = async (event: APIGatewayEvent) => {
   const body: ParsedBody = JSON.parse(event.body!);
   const { type, data } = body;
-  const isVerified = authenticate(event);
+  const isVerified = authenticate(event, nacl);
 
   if (!isVerified) {
     return {
@@ -124,35 +54,6 @@ export const main = async (event: APIGatewayEvent) => {
       return JSON.stringify({ type: 4, data: str });
     }
 
-    function createSelectMenu(games: GetActiveGameResponse) {
-      const components = games.data.map((game) => ({
-        label: `${teams[game.red_side.team_name]} vs ${
-          teams[game.blue_side.team_name]
-        }`,
-        value: game.game_id,
-        description: `${teams[game.red_side.team_name]} vs ${
-          teams[game.blue_side.team_name]
-        }`,
-      }));
-
-      return {
-        content: "Select game(s) to close voting for.",
-        components: [
-          {
-            type: 1,
-            components: [
-              {
-                type: 3,
-                custom_id: "close-voting-selection",
-                options: components,
-                placeholder: "Choose.",
-              },
-            ],
-          },
-        ],
-      };
-    }
-
     if (data.name === "close-voting") {
       const pl = await Game.getActiveGames();
       if (!pl.data.length) {
@@ -165,7 +66,15 @@ export const main = async (event: APIGatewayEvent) => {
       }
       return JSON.stringify({
         type: 4,
-        data: createSelectMenu(pl),
+        data: createVotingSelectMenu({
+          games: pl,
+          title: "Select game to close voting for.",
+          placeholder: "Games",
+          customId: "close-voting-selection",
+        }),
+      });
+    }
+
       });
     }
   }
@@ -189,7 +98,7 @@ export const main = async (event: APIGatewayEvent) => {
         gameId,
       };
       try {
-        sendToSQS(voteData);
+        sendToVotesQueue(voteData);
       } catch (e) {
         console.error("error sending to queue records: ", e);
         throw e;
